0a462b05c73ab1b96fc65086e4315ed3fbf51fdd
Huzair13 <ahamedhuzair13@gmail.com>
2025-08-11 18:08:08 +0530

    job edited

---
+++ jobs/job_6/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='utils',
+    version='0.1',
+    packages=find_packages(),
+    description='Glue utils for job_6',
+    include_package_data=True,
+    package_data={'utils': ['vendor/**/*']},
+)
