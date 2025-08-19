7eb528dce3f3cdc2ee8a877e35a141efd4435a6f
Huzair13 <ahamedhuzair13@gmail.com>
2025-08-19 12:56:37 +0530

    updated

--- sp_jobs/jobs_1/main.tf
+++ sp_jobs/jobs_1/main.tf
@@ -1,5 +1,20 @@
+terraform {
+  backend "s3" {}
+}
+
+terraform {
+  required_version = ">= 1.5.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" 
+    }
+  }
+}
+
 module "redshift_procs" {
-  source             = "./modules/redshift-procs"
+  source             = "../../terraform_modules/redshift-procs"
   database           = var.database
   workgroup_name     = var.workgroup_name 
   secret_arn         = var.secret_arn
--- sp_jobs/jobs_1/terraform.tfvars
+++ sp_jobs/jobs_1/terraform.tfvars
@@ -1,3 +1,3 @@
-database=""
-workgroup_name=""
-secret_arn=""
+database="dev"
+workgroup_name="siriusxm-test-wg"
+secret_arn="arn:aws:secretsmanager:us-east-1:253157412491:secret:siriusxm-redshift-secret-S4Xv16"
